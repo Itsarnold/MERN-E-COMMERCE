60b9313445ac5adb85b3afe0da6e241537c5ba67
nianod <arnoldkk422@gmail.com>
2026-02-23 20:52:11 +0300

    Added lazy loading to reduce server pressure

--- Client/src/Pages/Products.tsx
+++ Client/src/Pages/Products.tsx
@@ -4,19 +4,27 @@ import axios from "axios";
 import useSound from "use-sound";
 import { useCart } from "../CartContext";
 import Categories from "../Components/categories";
+import { Loader2 } from "lucide-react";
 
 const AllProducts = () => {
+
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<String | null>(null)
     const { setCartCount, searchItem, setCartItems } = useCart();
     const [allProducts, setAllProducts] = useState<Product[]>([])
     
       const fetchProducts = async () => {
     try {
+      setLoading(true)
+      setError(null)
       const apiUrl = import.meta.env.VITE_HEROKU_URL;
       const response = await axios.get<Product[]>(`${apiUrl}/api/products`);
       setAllProducts(response.data);
       //console.log(response.data)
     } catch (error) {
       console.error("error occurred", error);
+    } finally{
+      setLoading(false)
     }
   };
 
@@ -32,6 +40,12 @@ const AllProducts = () => {
   );
 
   const [play] = useSound("/mixkit-mouse-click-close-1113.wav")
+
+  if(loading) {
+    return(
+      <div className="flex justify-center items-center"><Loader2 className="h-15 w-15 animate-spin" /> <br /> LODING...</div>
+    )
+  }
   
   return (
     <div className="p-3">
